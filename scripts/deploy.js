0f561f6fa062713bdc4bd45238f533701d6c2a0e
Carl Eric Doromal <cedoromal@gmail.com>
2023-07-03 09:38:24 +0000

    Added BasicMath Functionality

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,19 +6,7 @@
 // global scope, and execute the script.
 import hre from "hardhat";
 
-const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-const unlockTime = currentTimestampInSeconds + 60;
+const basicMath = await hre.ethers.deployContract("BasicMath");
+await basicMath.waitForDeployment();
 
-const lockedAmount = hre.ethers.parseEther("0.001");
-
-const lock = await ethers.deployContract("Lock", [unlockTime], {
-  value: lockedAmount,
-});
-
-await lock.waitForDeployment();
-
-console.log(
-  `Lock with ${ethers.formatEther(
-    lockedAmount
-  )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-);
+console.log(`BasicMath contract deployed to ${await basicMath.getAddress()}`);
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
+import basicMath from "../artifacts/contracts/BasicMath.sol/BasicMath.json";
 import "./App.css";
 import {
   ThemeProvider,
@@ -22,6 +23,13 @@ function App() {
 
   const [ethWallet, setEthWallet] = useState(undefined);
   const [account, setAccount] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+
+  const contractAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"; //MODIFY THIS TO POINT TO THE ADDRESS WHERE YOU DEPLOYED THE CONTRACT
+
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState(0);
 
   const getWallet = async () => {
     if (window.ethereum) {
@@ -53,8 +61,41 @@ function App() {
     handleAccount(accounts);
 
     // once wallet is set we can get a reference to our deployed contract
-    //getATMContract();
+    getContract();
   };
+
+  const getContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethWallet);
+    const signer = provider.getSigner();
+    const basicMathContract = new ethers.Contract(
+      contractAddress,
+      basicMath.abi,
+      signer
+    );
+
+    setContract(basicMathContract);
+  };
+
+  const add = async (num1, num2) => {
+    if (!contract) return;
+    let tx = await contract.add(num1, num2);
+    await tx.wait();
+    updateResult();
+  };
+
+  const subtract = async (num1, num2) => {
+    if (!contract) return;
+    let tx = await contract.subtract(num1, num2);
+    await tx.wait();
+    updateResult();
+  };
+
+  const updateResult = async () => {
+    if (!contract) return;
+    setResult(Number(await contract.getLatestResult()));
+  };
+
+  updateResult();
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -63,49 +104,52 @@ function App() {
       {useEffect(() => {
         getWallet();
       }, [])}
-      {!account ? (
-        <>
-          <Typography variant="h1" className="p-12">
-            Please connect your wallet to proceed
+
+      <div className="flex justify-center w-full">
+        <div className="flex flex-col gap-3 p-3 w-full md:w-1/2">
+          <Typography variant="h3"> Latest Result </Typography>
+          <Typography variant="h1" className="w-full">
+            {account? result : "Unavailable"}
           </Typography>
-          <Button variant="outlined" onClick={() => connectAccount()}>
-            {" "}
-            Connect Wallet{" "}
-          </Button>
-        </>
-      ) : (
-        <>
-          <div className="flex justify-center w-full">
-            <div className="flex flex-col gap-3 p-3 w-full md:w-1/2">
-              <Typography variant="h1" id="answer" className="w-full">
-                0
-              </Typography>
-              <div className="flex justify-center gap-3">
-                <TextField
-                  variant="outlined"
-                  type="number"
-                  placeholder="Number 1"
-                  className="w-full"
-                />
-                <TextField
-                  variant="outlined"
-                  type="number"
-                  placeholder="Number 2"
-                  className="w-full"
-                />
-              </div>
-              <div className="flex justify-center gap-3">
-                <Button variant="contained" className="w-full">
-                  Add
-                </Button>
-                <Button variant="contained" className="w-full">
-                  Subtract
-                </Button>
-              </div>
-            </div>
+          <div className="flex justify-center gap-3">
+            <TextField
+              variant="outlined"
+              type="number"
+              placeholder="Number 1"
+              value={num1}
+              onChange={(event) => setNum1(Number(event.target.value))}
+              className="w-full"
+            />
+            <TextField
+              variant="outlined"
+              type="number"
+              placeholder="Number 2"
+              value={num2}
+              onChange={(event) => setNum2(Number(event.target.value))}
+              className="w-full"
+            />
           </div>
-        </>
-      )}
+          <div className="flex justify-center gap-3">
+            <Button
+              variant="contained"
+              onClick={() => add(num1, num2)}
+              className="w-full"
+            >
+              Add
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => subtract(num1, num2)}
+              className="w-full"
+            >
+              Subtract
+            </Button>
+          </div>
+          <Button variant="contained" onClick={connectAccount}>
+            Connect Wallet
+          </Button>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
